517ab1e62f9ab0bc1af4e2b1ca2fba648d86793e
Park2540 <paklovethai.54@gmail.com>
2021-12-22 13:38:26 +0700

    Start

---
+++ server.js
@@ -0,0 +1,52 @@
+import {ApolloServer, gql} from 'apollo-server';
+//import { ApolloServer } from 'apollo-server-express';
+const users =[
+    {name: "prak", sex: "M"},
+    {name: "ploy", sex: "w"},
+    {name: "bank", sex: "M"},
+];
+
+//schema
+const typeDefs = gql`
+    type Query{
+        hello: String
+        hi: String
+        users:[User]
+        user(name: String): User
+    }
+    type User{
+        name: String
+        sex: String
+    }
+`;
+//resolver
+const resolvers ={
+    Query:{
+        hello: (parent, args, context, info) =>{
+            return "world";
+        },
+        hi: (parent, args, context, info) =>{
+            return "62021203";
+        },
+        users: (parent, args, context, info) =>{
+            return users;
+        },
+        user: (parent, args, context, info) =>{
+            return users.find( user => user.name ===args.name);
+        }
+
+    }
+};
+
+
+//function apollo-server
+const startApolloServer = async (typeDefs, resolvers) =>{
+    const server = new ApolloServer({typeDefs, resolvers});
+    const {url} = await server.listen();
+    console.log(`Server ready at ${url}`);
+
+};
+
+
+//call function
+startApolloServer(typeDefs, resolvers);
